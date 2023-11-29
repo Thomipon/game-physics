15ab6a3ccd9f6459d1fc19c984c428f4329b978e
Thomipon <38535540+Thomipon@users.noreply.github.com>
2023-11-29 16:38:45 +0100

    boilerplate stuff

---
+++ Simulations/RigidBodySystemSimulator.cpp
@@ -0,0 +1,115 @@
+﻿#include "RigidBodySystemSimulator.h"
+
+Mat4 box::get_transform() const
+{
+    const Mat4 scale_mat{
+        size.x, 0., 0., 0.,
+        0., size.y, 0., 0.,
+        0., 0., size.z, 0.,
+        0., 0., 0., 1.
+    };
+
+    const Mat4 translation_mat{
+        0., 0., 0., center_position.x,
+        0., 0., 0., center_position.y,
+        0., 0., 0., center_position.z,
+        0., 0., 0., 1.
+    };
+    return scale_mat * rotation.getRotMat() * translation_mat;
+}
+
+RigidBodySystemSimulator::RigidBodySystemSimulator()
+    : Simulator(), m_mouse(), m_trackmouse(), m_oldtrackmouse()
+{
+}
+
+const char* RigidBodySystemSimulator::getTestCasesStr()
+{
+    return "Single Step,Single Body,Two Bodies,Complex";
+}
+
+void RigidBodySystemSimulator::initUI(DrawingUtilitiesClass* DUC)
+{
+    this->DUC = DUC;
+}
+
+void RigidBodySystemSimulator::reset()
+{
+    m_mouse.x = m_mouse.y = 0;
+    m_trackmouse.x = m_trackmouse.y = 0;
+    m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
+}
+
+void RigidBodySystemSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext)
+{
+    DUC->setUpLighting(Vec3{}, Vec3{.4}, 100, 0.6 * Vec3{0.97, 0.86, 1});
+    for (const auto& body : bodies_)
+    {
+        DUC->drawRigidBody(body.get_transform());
+    }
+}
+
+void RigidBodySystemSimulator::notifyCaseChanged(int testCase)
+{
+}
+
+void RigidBodySystemSimulator::externalForcesCalculations(float timeElapsed)
+{
+}
+
+void RigidBodySystemSimulator::simulateTimestep(float timeStep)
+{
+}
+
+void RigidBodySystemSimulator::onClick(int x, int y)
+{
+    m_trackmouse.x = x;
+    m_trackmouse.y = y;
+}
+
+void RigidBodySystemSimulator::onMouse(int x, int y)
+{
+    m_oldtrackmouse.x = x;
+    m_oldtrackmouse.y = y;
+    m_trackmouse.x = x;
+    m_trackmouse.y = y;
+}
+
+int RigidBodySystemSimulator::getNumberOfRigidBodies() const
+{
+    return bodies_.size();
+}
+
+Vec3 RigidBodySystemSimulator::getPositionOfRigidBody(const int i)
+{
+    return bodies_.at(i).center_position;
+}
+
+Vec3 RigidBodySystemSimulator::getLinearVelocityOfRigidBody(const int i)
+{
+    return bodies_.at(i).linear_velocity;
+}
+
+Vec3 RigidBodySystemSimulator::getAngularVelocityOfRigidBody(const int i)
+{
+    return bodies_.at(i).angular_velocity;
+}
+
+void RigidBodySystemSimulator::applyForceOnBody(int i, Vec3 loc, Vec3 force)
+{
+}
+
+void RigidBodySystemSimulator::addRigidBody(Vec3 position, Vec3 size, int mass)
+{
+    bodies_.emplace_back(position, size, Quat{0., 0., 0.}, Vec3{0.}, Vec3{0.}, mass);
+}
+
+void RigidBodySystemSimulator::setOrientationOf(const int i, const Quat& orientation)
+{
+    bodies_.at(i).rotation = orientation;
+}
+
+void RigidBodySystemSimulator::setVelocityOf(const int i, const Vec3& velocity)
+{
+    bodies_.at(i).linear_velocity = velocity;
+}
--- Simulations/RigidBodySystemSimulator.h
+++ Simulations/RigidBodySystemSimulator.h
@@ -1,46 +1,72 @@
-#ifndef RIGIDBODYSYSTEMSIMULATOR_h
-#define RIGIDBODYSYSTEMSIMULATOR_h
+#pragma once
+
 #include "Simulator.h"
+#include <vector>
 //add your header for your rigid body system, for e.g.,
 //#include "rigidBodySystem.h" 
 
 #define TESTCASEUSEDTORUNTEST 2
 
-class RigidBodySystemSimulator:public Simulator{
-public:
-	// Construtors
-	RigidBodySystemSimulator();
-	
-	// Functions
-	const char * getTestCasesStr();
-	void initUI(DrawingUtilitiesClass * DUC);
-	void reset();
-	void drawFrame(ID3D11DeviceContext* pd3dImmediateContext);
-	void notifyCaseChanged(int testCase);
-	void externalForcesCalculations(float timeElapsed);
-	void simulateTimestep(float timeStep);
-	void onClick(int x, int y);
-	void onMouse(int x, int y);
+struct box
+{
+    box(const Vec3& center, const Vec3& size, const Quat& rotation, const Vec3& linear_velocity,
+        const Vec3& angular_velocity, const double& mass)
+        : center_position(center),
+          size(size),
+          rotation(rotation.unit()),
+          linear_velocity(linear_velocity),
+          angular_velocity(angular_velocity),
+          mass(mass)
+    {
+    }
 
-	// ExtraFunctions
-	int getNumberOfRigidBodies();
-	Vec3 getPositionOfRigidBody(int i);
-	Vec3 getLinearVelocityOfRigidBody(int i);
-	Vec3 getAngularVelocityOfRigidBody(int i);
-	void applyForceOnBody(int i, Vec3 loc, Vec3 force);
-	void addRigidBody(Vec3 position, Vec3 size, int mass);
-	void setOrientationOf(int i,Quat orientation);
-	void setVelocityOf(int i, Vec3 velocity);
+    Vec3 center_position;
+    Vec3 size;
+    Quat rotation;
+    Vec3 linear_velocity;
+    Vec3 angular_velocity;
+    double mass;
+
+    Mat4 get_transform() const;
+};
+
+class RigidBodySystemSimulator : public Simulator
+{
+public:
+    // Construtors
+    RigidBodySystemSimulator();
+
+    // Functions
+    const char* getTestCasesStr() override;
+    void initUI(DrawingUtilitiesClass* DUC) override;
+    void reset() override;
+    void drawFrame(ID3D11DeviceContext* pd3dImmediateContext) override;
+    void notifyCaseChanged(int testCase) override;
+    void externalForcesCalculations(float timeElapsed) override;
+    void simulateTimestep(float timeStep) override;
+    void onClick(int x, int y) override;
+    void onMouse(int x, int y) override;
+
+    // ExtraFunctions
+    int getNumberOfRigidBodies() const;
+    Vec3 getPositionOfRigidBody(int i);
+    Vec3 getLinearVelocityOfRigidBody(int i);
+    Vec3 getAngularVelocityOfRigidBody(int i);
+    void applyForceOnBody(int i, Vec3 loc, Vec3 force);
+    void addRigidBody(Vec3 position, Vec3 size, int mass);
+    void setOrientationOf(int i, const Quat& orientation);
+    void setVelocityOf(int i, const Vec3& velocity);
 
 private:
-	// Attributes
-	// add your RigidBodySystem data members, for e.g.,
-	// RigidBodySystem * m_pRigidBodySystem; 
-	Vec3 m_externalForce;
+    // Attributes
+    // add your RigidBodySystem data members, for e.g.,
+    // RigidBodySystem * m_pRigidBodySystem; 
+    Vec3 m_externalForce;
 
-	// UI Attributes
-	Point2D m_mouse;
-	Point2D m_trackmouse;
-	Point2D m_oldtrackmouse;
-	};
-#endif
+    // UI Attributes
+    Point2D m_mouse;
+    Point2D m_trackmouse;
+    Point2D m_oldtrackmouse;
+
+    std::vector<box> bodies_;
+};
